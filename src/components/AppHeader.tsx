886f368a2257c894171e9aa3a0b17cac5223cb90
Zahra <bghzahra@outlook.com>
2022-03-07 19:10:44 +0000

    added handle function that console logged H1 title in uppercase every time it was clicked

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,8 +2,13 @@ interface AppHeaderProps {
   title: string;
 }
 
+
 function AppHeader({ title }: AppHeaderProps): JSX.Element {
-  return <h1>{title}</h1>;
+
+  function header(){
+     console.log(title.toUpperCase())
+  }
+  return (<h1 onClick={header}>{title} </h1>)
 }
 
 export default AppHeader;
